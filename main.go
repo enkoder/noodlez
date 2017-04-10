128246948ec85b34d7084701b367a2878de220ba
Kodie <kodiegoodwin@gmail.com>
2017-04-09 22:14:03 -0700

    Use full variable names

--- main.go
+++ main.go
@@ -24,19 +24,19 @@ type Strip struct {
 }
 
 type Noodle struct {
-	b      hwio.Pin
-	c      *opc.Client
-	m      *opc.Message
-	Strips []Strip
+	button  hwio.Pin
+	client  *opc.Client
+	message *opc.Message
+	Strips  []Strip
 }
 
 func (n *Noodle) Render() error {
 	for s := range n.Strips {
 		for led := 0; led < LEDsPerStrip; led++ {
-			n.m.SetPixelColor((s*LEDsPerChannel)+led, n.Strips[s].R[led], n.Strips[s].G[led], n.Strips[s].B[led])
+			n.message.SetPixelColor((s*LEDsPerChannel)+led, n.Strips[s].R[led], n.Strips[s].G[led], n.Strips[s].B[led])
 		}
 	}
-	return n.c.Send(n.m)
+	return n.client.Send(n.message)
 }
 
 func (n *Noodle) Solid(r uint8, g uint8, b uint8) error {
@@ -68,38 +68,38 @@ func (n *Noodle) Blue() error {
 }
 
 func (n *Noodle) ButtonPressed() (bool, error) {
-	value, err := hwio.DigitalRead(n.b)
+	value, err := hwio.DigitalRead(n.button)
 	return value == 1, err
 }
 
 func NewNoodle(button_gpio string) (*Noodle, error) {
-	b, err := hwio.GetPin(ButtonPin)
+	button, err := hwio.GetPin(ButtonPin)
 	if err != nil {
 		return nil, fmt.Errorf("Error during button init: %v\n", err)
 	}
 
-	err = hwio.PinMode(b, hwio.INPUT)
+	err = hwio.PinMode(button, hwio.INPUT)
 	if err != nil {
 		return nil, fmt.Errorf("Error during button mode set: %v\n", err)
 	}
 
 	// Create a client
-	c := opc.NewClient()
-	err = c.Connect("tcp", "localhost:7890")
+	client := opc.NewClient()
+	err = client.Connect("tcp", "localhost:7890")
 	if err != nil {
 		return nil, err
 	}
 
-	var m *opc.Message
-	m = opc.NewMessage(0)
-	m.SetLength(uint16(LEDsPerChannel * Strips * 3))
+	var message *opc.Message
+	message = opc.NewMessage(0)
+	message.SetLength(uint16(LEDsPerChannel * Strips * 3))
 
 	strips := make([]Strip, Strips)
 	for i := 0; i < Strips; i++ {
 		strips[i] = Strip{}
 	}
 
-	return &Noodle{b, c, m, strips}, nil
+	return &Noodle{button, client, message, strips}, nil
 }
 
 func main() {
